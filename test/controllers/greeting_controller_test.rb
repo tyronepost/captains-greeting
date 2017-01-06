7c2487b0d7c67d3cab5baa7e49a959ba272dde48
tyronepost <tyrone.post.a@gmail.com>
2017-01-05 21:02:27 -0500

    first commit and greeting controller

---
+++ test/controllers/greeting_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class GreetingControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get greeting_index_url
+    assert_response :success
+  end
+
+end
