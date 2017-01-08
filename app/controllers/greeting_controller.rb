78375782bc1c019df8a54fb39fbc881b5294587d
tyronepost <tyrone.post.a@gmail.com>
2017-01-08 17:04:56 -0500

    implemented basic rerendering entire page

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -1,4 +1,10 @@
 class GreetingController < ApplicationController
   def index
+    @name = 'Picard'
+  end
+
+  def refresh 
+    @name = 'Kirk'
+    render "greeting/index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'greeting/index'
+  post 'greeting/index' => 'greeting#refresh'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
